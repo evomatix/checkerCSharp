fa32cc0d3728a64fdaeace7ac73938a8230b54f3
Vimukthi Hewapathirana <vimunet@gmail.com>
2022-12-13 20:01:58 +0530

    added excel data source

---
+++ EvomatixChecker/DataSource/ExcelDataSources.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EvomatixTester.DataSource;
+using LumenWorks.Framework.IO.Csv;
+using NUnit.Framework;
+
+namespace EvomatixTester.DataDriven.DataSource
+{
+    public class ExcelDataSources
+    {
+        private IEnumerable<String[]> userData()
+        {
+            ExcelManager excel = new ExcelManager();
+            //change the path accordingly
+            excel.openWorkBook("/Users/vdhhewapathirana/Storage/Projects/evomatix/checkerCSharp/EvomatixChecker/Data/loginData.xlsx", "Data");
+            List<Dictionary<string, object>> data = excel.readExcelWithHeaders();
+
+            foreach (Dictionary<string, object> record in data)
+            {
+                String username = (String)record["username"];
+                String password = (String)record["password"];
+
+                yield return new[] { username, password };
+            }
+        }
+    }
+}
+
